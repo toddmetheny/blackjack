238cade8f58dab3809245db64a69848fc1eaa508
Todd Metheny <todd.metheny@gmail.com>
2015-05-29 11:52:22 -0400

    adjusting value for an ace if the players total rises above 21

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,27 @@
+# class Splits < Blackjack
+#   def initialize(card1, card2)
+#     @card1 = card1
+#     @card2 = card2
+#     @hand1 = [@card1]
+#     @hand2 = [@card2]
+#   end
+
+#   def play_first_hand
+#     @second_card = deal_card()
+#     @hand1 << @second_card
+#   end
+
+#   def play_second_hand
+#     @
+#   end
+# end
+
+# class DoubleDown < Blackjack
+#   def initialize
+
+#   end
+# end
+
 class Blackjack
   attr_accessor :deck, :player_hand
   
@@ -6,11 +30,11 @@ class Blackjack
   def initialize
     suit = (1..13).to_a
     @deck = suit * 4
-    p @deck.count
     @player_hand = []
     @dealer_hand = []
     @busted = false
     @dealer_busted = false
+    puts "FYI: cards will be reshuffled after every hand."
   end
 
   def bankroll
@@ -47,41 +71,83 @@ class Blackjack
     @dealer_upcard = deal_card()
 
     @player_hand << value(@player_card1)
+    
     @player_hand << value(@player_card2)
 
     @dealer_hand << value(@dealer_upcard)
     @dealer_hand << value(@dealer_downcard)
 
+    # @split = false
+
     game_play
   end
 
-  def split
-    puts "You got #{name(@player_card1)} and #{name(@player_card2)}, which have the same value, would you like to split?"
-    yes_or_no = gets.chomp.downcase
-    if yes_or_no == "yes"
-      ####################
-    end
-  end
+  # def split
+  #   @split = true
+  #   puts "You got #{name(@player_card1)} and #{name(@player_card2)}, which have the same value, would you like to split?"
+  #   yes_or_no = gets.chomp.downcase
+  #   if yes_or_no == "yes"
+  #     ####################
+  #     @new_first_hand = [@player_card1]
+  #     @second_card_first_hand = deal_card()
+  #     @new_first_hand << @second_card_first_hand
+  #     @new_second_hand = [@player_card2]
+  #     @second_card_second_hand = deal_card()
+  #     @new_second_hand << @second_card_second_hand
+  #     after_split_first(@new_first_hand)
+  #     after_split_second(@new_second_hand)
+  #   else 
+  #     puts "The dealer is showing " + name(@dealer_upcard)
+  #     puts "Would you like to hit or stay? (reply 'hit' OR 'stay')"
+  #     @answer = gets.chomp
+  #   end
+  # end
+
+  # def after_split_first(hand)
+  #   puts "Let's handle this hand first."
+  #   puts "You were dealt #{@second_card_first_hand} to go with #{@player_card1} for a total of #{player_total(@new_first_hand)}."
+  #   puts "The dealer is showing #{name(@dealer_upcard)}."
+  #   puts "Would you like to hit or stay? (Enter: 'hit' or 'stay')"
+  #   first_split = gets.chomp.downcase
+  #   if first_split == 'hit'
+  #     split_card_1 = deal_card()
+  #     @new_first_hand << split_card_1
+  #     puts "your new total for this hand is #{player_total(@new_first_hand)}"
+  #     hit_question()
+  #     hit_or_stay(@new_first_hand)
+  #   else
+  #     puts ""
+  #   end
+  # end
+
+  # def after_split_second(hand)
+  # end
 
   def game_play
     puts "You have a " + name(@player_card1) + " and a " + name(@player_card2) +
-    " for a total of " + player_total.to_s
+    " for a total of " + player_total(@player_hand).to_s
 
-    if player_total == 21
+    if player_total(@player_hand) == 21
       puts "You got blackjack!"
       pick_winner
       unless (@dealer_upcard == 1 || @dealer_upcard >= 10) 
         puts "You win!"
       end
-    elsif value(@player_card1) == value(@player_card2)
-      split
+    # elsif player_total <= 11
+    #   puts "You have #{player_total}. Do you want to double down?"
+    #   ####################################
+    # elsif value(@player_card1) == value(@player_card2)
+    #   split
     else
       puts "The dealer is showing " + name(@dealer_upcard)
-      puts "Would you like to hit or stay? (reply 'hit' OR 'stay')"
-      @answer = gets.chomp
-
-      hit_or_stay()
+      hit_question
+      hit_or_stay(@player_hand)
     end
+  end
+
+  def hit_question
+    puts "Would you like to hit or stay? (reply 'hit' OR 'stay')"
+    @answer = gets.chomp
   end
 
   def deal_card
@@ -112,30 +178,42 @@ class Blackjack
     end
   end
 
-  def player_total
-    @player_hand.inject(:+)
-    # if total > 21 && @player_hand.include(11)
+  def player_total(hand)
+    # @player_hand.inject(:+)
+    total = hand.inject(:+)
+    if total > 21 && hand.include?(11)
+      hand.delete(11)
+      hand << 1
+      p "ace_adjustment: #{hand}"
+      total = hand.inject(:+)
+    else 
+      total = hand.inject(:+)
+    end
+    total
   end
+
+  # def ace_case
+  # end
 
   def dealer_total
     # value(@dealer_upcard) + value(@dealer_downcard)
     @dealer_hand.inject(:+)
   end
 
-  def hit_or_stay
-    while @answer.downcase != 'stay' && player_total < 21
+  def hit_or_stay(hand)
+    while @answer.downcase != 'stay' && player_total(hand) < 21
       if @answer.downcase == 'hit'
         @another_card = deal_card
-        @player_hand << @another_card
+        hand << @another_card
         puts "You drew a " + name(@another_card)
         
-        p player_total
-        puts "Your new total is " + player_total.to_s
-        if player_total == 21
+        p player_total(hand)
+        puts "Your new total is " + player_total(hand).to_s
+        if player_total(hand) == 21
           @answer = 'stay'
-        elsif player_total > 21
+        elsif player_total(hand) > 21
           @answer = 'stay'
-          busted?
+          player_busted?(hand)
         else
           puts "Would you like to hit again or stay?"
           @answer = gets.chomp
@@ -153,14 +231,16 @@ class Blackjack
     dealer_hits_or_stays
   end
 
-  def busted?
-    if player_total > 21
+  def player_busted?(hand)
+    if player_total(hand) > 21
       puts "You busted sucker!"
       @busted = true
     else
       @busted = false
     end
+  end
 
+  def dealer_busted?
     if dealer_total > 21
       puts "Dealer busted"
       @dealer_busted = true
@@ -177,7 +257,7 @@ class Blackjack
       puts dealer_total
       puts "The dealer drew a(n) " + name(another_card) + " for a new total of " + dealer_total.to_s
     end
-    busted?
+    dealer_busted?
     pick_winner
   end
 
@@ -191,13 +271,13 @@ class Blackjack
     end
   end
 
-  def pick_winner
-    puts "The player has " + player_total.to_s
+  def pick_winner(hand=@player_hand)
+    puts "The player has " + player_total(hand).to_s
 
-    if(player_total == dealer_total && @busted == false)
+    if(player_total(hand) == dealer_total && @busted == false)
       puts "Dealer and player tie. It's a push."
-    elsif(player_total > dealer_total && @busted == false) || (@busted == false && @dealer_busted == true)
-      puts "Player wins " + player_total.to_s + " to " + dealer_total.to_s
+    elsif(player_total(hand) > dealer_total && @busted == false) || (@busted == false && @dealer_busted == true)
+      puts "Player wins " + player_total(hand).to_s + " to " + dealer_total.to_s
       if @gambler
         @@bankroll += @amount
         puts "you won #{@amount}"
@@ -208,7 +288,7 @@ class Blackjack
         end
       end
     else
-      puts "Dealer wins " + dealer_total.to_s + " to " + player_total.to_s
+      puts "Dealer wins " + dealer_total.to_s + " to " + player_total(hand).to_s
       if @gambler
         @@bankroll -= @amount
         puts "you lost #{@amount}"
